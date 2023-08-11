9b81f5ba34af7b4b0ebbb4f0b48b93729996737d
Henry <hogbunike@gmail.com>
2023-08-11 02:44:59 +0100

    initial setup, created apps for core, user and store

---
+++ ecommerce/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from core.views import frontpage, about
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', frontpage, name='frontpage'),
+    path('about', about, name='about'),
+]
